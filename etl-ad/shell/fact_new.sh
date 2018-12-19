1567c1d99ea268fdfc9369ba92b7e24f0c03786f
github <customme@163.com>
2018-12-19 14:54:09 +0800

    完善注释

--- etl-ad/shell/fact_new.sh
+++ etl-ad/shell/fact_new.sh
@@ -3,11 +3,16 @@
 # Date: 2018-12-11
 # Author: superz
 # Description: 从hdfs上获取访问日志(json格式),分析得到新增用户并写入mysql表
+# 调度系统变量
+#   log_path     任务日志目录
+#   prev_day     run_time前一天(yyyyMMdd)
+#   prev_day1    run_time前一天(yyyy-MM-dd)
 # 任务扩展属性:
 # hdfs_dir        访问日志hdfs根目录
 # product_code    产品编码
-# ad_db_id        数据库id
+# ad_db_id        广告数据库id
 # tbl_new         新增用户表名(默认为fact_new_$product_code)
+# bak_count       备份表保留个数
 # 任务运行时参数:
 # start_date      开始日期(默认为run_time的前一天)
 # end_date        结束日期(默认为start_date)
@@ -28,7 +33,7 @@ function get_data()
             hdfs dfs -get $hdfs_dir/$product_code/$the_date $data_path
         fi
 
-        # json格式转列表格式
+        # json格式转扁平格式
         ls $data_path/$the_date | while read file; do
             debug "Format file: $data_path/$the_date/$file"
             sed 's/\":\"\|\",\"/\t/g;s/{\"\|\"}//g' $data_path/$the_date/$file |
@@ -83,25 +88,32 @@ function load_data()
     # 设置数据库
     set_db $ad_db_id
 
+    local his_day=`date +%Y%m%d -d "$the_day $bak_count day ago"`
+
     echo "LOCK TABLES $tbl_new WRITE;
     DROP TABLE IF EXISTS ${tbl_new}_$prev_day;
     RENAME TABLE $tbl_new TO ${tbl_new}_$prev_day;
     CREATE TABLE $tbl_new LIKE ${tbl_new}_$prev_day;
     LOAD DATA LOCAL INFILE '$file_result' INTO TABLE $tbl_new (aid, channel_code, init_area, area, init_ip, ip, create_time, update_time);
+    DROP TABLE IF EXISTS ${tbl_new}_$his_day;
     UNLOCK TABLES;
     " | exec_sql
 }
 
 function execute()
 {
-    # 开始/结束日期
+    # 开始日期
     start_date=`awk -F '=' '$1 == "start_date" {print $2}' $log_path/run_params`
     start_date=${start_date:-$prev_day1}
+    # 结束日期
     end_date=`awk -F '=' '$1 == "end_date" {print $2}' $log_path/run_params`
     end_date=${end_date:-$start_date}
 
     # 新增用户表
     tbl_new=${tbl_new:-fact_new_$product_code}
+
+    # 备份表保留个数
+    bak_count=${bak_count:-3}
 
     # 从hdfs获取并格式化合并后的文件
     file_visit=$data_path/visit.hdfs
